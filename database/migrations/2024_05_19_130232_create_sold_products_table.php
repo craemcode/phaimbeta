43b9d1095629614560d9ec771204e1828bf3da0f
Zachariah Ndungu <ndunguzack13@gmail.com>
2024-05-19 16:17:42 +0300

    made the migration for sold products table. sales working

---
+++ database/migrations/2024_05_19_130232_create_sold_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sold_products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained(
+                table:'products', indexName: 'product_id'
+            );
+            $table->foreignId('sales_id')->constrained(
+                table:'sales', indexName: 'sales_id'
+            );
+            $table->integer('product_quantity');
+            $table->integer('product_selling_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sold_products');
+    }
+};
