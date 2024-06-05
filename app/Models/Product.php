695bcde5fbd31acd97496d051dc781e175606f34
Zachariah Ndungu <ndunguzack@gmail.com>
2024-06-05 14:09:23 +0300

    fixed bulk imports

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Product extends Model
 {
@@ -15,4 +16,13 @@ class Product extends Model
         'units',
         'stock_id',
     ];
+
+    public function stock(){
+        return $this->belongsTo(Stock::class);
+    }
+
+    public function restocked_product():HasMany
+    {
+        return $this->hasMany(RestockedProduct::class);
+    }
 }
